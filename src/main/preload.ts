725123a9fb4689b1c4020d7a460bd06d5b92acdf
Vince Au <vinceau09@gmail.com>
2021-12-28 00:19:07 +1100

    chore: enable declaring preload in typescript

---
+++ src/main/preload.ts
@@ -0,0 +1,23 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: {
+    myPing() {
+      ipcRenderer.send('ipc-example', 'ping');
+    },
+    on(channel: string, func: any) {
+      const validChannels = ['ipc-example'];
+      if (validChannels.includes(channel)) {
+        // Deliberately strip event as it includes `sender`
+        ipcRenderer.on(channel, (_event, ...args) => func(...args));
+      }
+    },
+    once(channel: string, func: any) {
+      const validChannels = ['ipc-example'];
+      if (validChannels.includes(channel)) {
+        // Deliberately strip event as it includes `sender`
+        ipcRenderer.once(channel, (_event, ...args) => func(...args));
+      }
+    },
+  },
+});
