91fac24e37ba7b29ce66872527976d4b24fea948
Vince Au <vinceau09@gmail.com>
2022-01-03 10:06:32 +1100

    feat: add csp policy (#3)

--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -9,6 +9,7 @@ import baseConfig from './webpack.config.base';
 import webpackPaths from './webpack.paths';
 import checkNodeEnv from '../scripts/check-node-env';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import CspHtmlWebpackPlugin from 'csp-html-webpack-plugin';
 
 // When an ESLint server is running, we can't set the NODE_ENV so we'll check if it's
 // at the dev webpack config is not accidentally run in a production environment
@@ -143,6 +144,11 @@ const configuration: webpack.Configuration = {
       env: process.env.NODE_ENV,
       isDevelopment: process.env.NODE_ENV !== 'production',
       nodeModules: webpackPaths.appNodeModulesPath,
+    }),
+
+    new CspHtmlWebpackPlugin({
+      'script-src': '',
+      'style-src': ["'unsafe-inline'"],
     }),
   ],
 
